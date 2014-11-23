2bcb8bf2a8e169f1285321076d58392c46c7f615
whfghfhw <whfcdfhw@163.com>
2014-11-23 23:14:46 +0800

    Initial version for web of my app.

---
+++ app-web/src/main/java/com/app/web/action/mytest/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * File Name: HelloWorld.java
+ * Author: hardyw 
+ * Created Date: Nov 22, 2014
+ * Description: 
+ * 
+ * Copyright (c) 2014, hardyw1024@163.com All Rights Reserved. 
+ */
+
+package com.app.web.action.mytest;
+
+public class HelloWorld {
+
+}
